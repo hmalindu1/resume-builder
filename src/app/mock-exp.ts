407899b51749a7f100668c33f429d0220ab3c88f
Hashan <hemachandra.hashan@gmail.com>
2022-05-30 22:03:27 +0530

    mock data set was created for experience

---
+++ src/app/mock-exp.ts
@@ -0,0 +1,19 @@
+import { Exp } from "./Exp";
+
+export const EXPS: Exp[] = [
+    {
+        id: 1,
+        company: 'LSEG',
+        position: 'Tech Lead',
+        period: 'June 2021 - Present',
+        time_frame: '1 Years',
+        projects: ['test1', 'test2', 'test3']
+    }, {
+        id: 2,
+        company: 'Synopsys',
+        position: 'Assoceate Tech Lead',
+        period: 'March 2018 - June 2021',
+        time_frame: '2 Years',
+        projects: ['test4', 'test5', 'test6']
+    }
+]
