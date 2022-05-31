b15872961c47bc72e3e64ea494ecfada42fe9578
Hashan <hemachandra.hashan@gmail.com>
2022-05-31 13:19:10 +0530

    delete_exp_s() was created using api url

--- src/app/services/exp.service.ts
+++ src/app/services/exp.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable, of } from "rxjs";
 import { Exp } from "../Exp";
-import { EXPS } from "../mock-exp";
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +13,10 @@ export class ExpService {
 
   get_exps(): Observable<Exp[]> {
     return this.http.get<Exp[]>(this.api_url)
+  }
+
+  delete_exp_s(exp: Exp): Observable<Exp> {
+    const url = `${this.api_url}/${exp.id}`;
+    return this.http.delete<Exp>(url);
   }
 }
