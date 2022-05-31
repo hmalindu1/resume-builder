e2c31a4f8b363834dab45e9bf81c80b84317b2f7
Hashan <hemachandra.hashan@gmail.com>
2022-05-31 11:15:07 +0530

    Close icon was added into experience item

--- src/app/components/exp-item/exp-item.component.ts
+++ src/app/components/exp-item/exp-item.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Exp } from 'src/app/Exp';
 import { faCircleDot } from '@fortawesome/free-regular-svg-icons';
 import { faCircle } from '@fortawesome/free-regular-svg-icons';
+import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-exp-item',
@@ -11,6 +12,7 @@ import { faCircle } from '@fortawesome/free-regular-svg-icons';
 export class ExpItemComponent implements OnInit {
   faCircleDot = faCircleDot;
   faCircle = faCircle;
+  faCircleXmark = faCircleXmark;
   @Input()
   exp!: Exp;
 
