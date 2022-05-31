aa87284f9f23b3f6b082709ddf027ae51c2891d0
Hashan <hemachandra.hashan@gmail.com>
2022-05-31 07:07:52 +0530

    mock experience file was changed in to dummy text

--- src/app/mock-exp.ts
+++ src/app/mock-exp.ts
@@ -7,13 +7,13 @@ export const EXPS: Exp[] = [
         position: 'Tech Lead',
         period: 'June 2021 - Present',
         time_frame: '1 Years',
-        projects: ['test1', 'test2', 'test3']
+        projects: ['Lorem Ipsum is simply dummy text of the printing and typesetting industry.', 'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s', 'when an unknown printer took a galley of type and scrambled it to make a type specimen book']
     }, {
         id: 2,
         company: 'Synopsys',
         position: 'Assoceate Tech Lead',
         period: 'March 2018 - June 2021',
         time_frame: '2 Years',
-        projects: ['test4', 'test5', 'test6']
+        projects: ['It has survived not only five centuries', 'but also the leap into electronic typesetting, remaining essentially unchanged', 'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages']
     }
 ]
