688c95e6583c2e272f068fdd611703e9c0a9601d
Hashan <hemachandra.hashan@gmail.com>
2022-05-31 13:18:14 +0530

    delete_exp() method was created and subscribed

--- src/app/components/exps/exps.component.ts
+++ src/app/components/exps/exps.component.ts
@@ -13,7 +13,11 @@ export class ExpsComponent implements OnInit {
   constructor(private exp_service: ExpService) { }
 
   ngOnInit(): void {
-    this.exp_service.get_exps().subscribe((exps) => this.exps = exps);
+    this.exp_service.get_exps().subscribe((exps) => (this.exps = exps));
+  }
+
+  delete_exp(exp: Exp) {
+    this.exp_service.delete_exp_s(exp).subscribe(() => (this.exps = this.exps.filter(e => e.id! !== exp.id)));
   }
 
 }
