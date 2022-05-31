5087f22d184151e041770b5b8f24a8a7a89989d9
Hashan <hemachandra.hashan@gmail.com>
2022-05-31 20:13:36 +0530

    Forms module was added into app.modules.ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
 
 
 import { AppComponent } from './app.component';
@@ -9,6 +10,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 import { ExpsComponent } from './components/exps/exps.component';
 import { ExpItemComponent } from './components/exp-item/exp-item.component';
+import { AddExpComponent } from './components/add-exp/add-exp.component';
 
 @NgModule({
   declarations: [
@@ -17,11 +19,13 @@ import { ExpItemComponent } from './components/exp-item/exp-item.component';
     ButtonComponent,
     ExpsComponent,
     ExpItemComponent,
+    AddExpComponent,
   ],
   imports: [
     BrowserModule,
     FontAwesomeModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
