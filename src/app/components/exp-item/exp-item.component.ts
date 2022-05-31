5e40fe1d719baee28e528a08ce3a26a06eebdcd0
Hashan <hemachandra.hashan@gmail.com>
2022-05-31 13:15:41 +0530

    Even is emitted using on_delete() method

--- src/app/components/exp-item/exp-item.component.ts
+++ src/app/components/exp-item/exp-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Exp } from 'src/app/Exp';
 import { faCircleDot } from '@fortawesome/free-regular-svg-icons';
 import { faCircle } from '@fortawesome/free-regular-svg-icons';
@@ -15,10 +15,15 @@ export class ExpItemComponent implements OnInit {
   faCircleXmark = faCircleXmark;
   @Input()
   exp!: Exp;
+  @Output() on_delete_exp_i: EventEmitter<Exp> = new EventEmitter;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  on_delete(exp: Exp) {
+    this.on_delete_exp_i.emit(exp)
   }
 
 }
