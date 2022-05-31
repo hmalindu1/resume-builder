c9679746ef16a6618bc5d49fcdca136af4a4d3ae
Hashan <hemachandra.hashan@gmail.com>
2022-05-31 09:41:01 +0530

    Imporing HttpClient and HttpHeaders to service

--- src/app/services/exp.service.ts
+++ src/app/services/exp.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable, of } from "rxjs";
 import { Exp } from "../Exp";
 import { EXPS } from "../mock-exp";
