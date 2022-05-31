5b20eebb2a9736ae6fcb6aa272c3d2a44e7b02b3
Hashan <hemachandra.hashan@gmail.com>
2022-05-31 09:42:19 +0530

    Placing modules in the same import list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 import { ExpsComponent } from './components/exps/exps.component';
 import { ExpItemComponent } from './components/exp-item/exp-item.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
