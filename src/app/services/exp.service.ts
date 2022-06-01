f2c752868d85642ccb68596c95cca0ae26e0ac8e
Hashan <hemachandra.hashan@gmail.com>
2022-06-01 22:04:51 +0530

    of removed

--- src/app/services/exp.service.ts
+++ src/app/services/exp.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { Observable, of } from "rxjs";
+import { Observable, } from "rxjs";
 import { Exp } from "../Exp";
 
 const http_options = {
