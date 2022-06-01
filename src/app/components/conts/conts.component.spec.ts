155c855e48a7e93393245b1fbe91f740b0c27dfc
Hashan <hemachandra.hashan@gmail.com>
2022-06-01 20:27:00 +0530

    Contact information parent element created with the service

---
+++ src/app/components/conts/conts.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ContsComponent } from './conts.component';
+
+describe('ContsComponent', () => {
+  let component: ContsComponent;
+  let fixture: ComponentFixture<ContsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ContsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ContsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/conts/conts.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ConstService } from "../../services/const.service";
+import { Cont } from "../../Cont";
+
+@Component({
+  selector: 'app-conts',
+  templateUrl: './conts.component.html',
+  styleUrls: ['./conts.component.css']
+})
+export class ContsComponent implements OnInit {
+  conts: Cont[] = []
+
+  constructor(private cont_service: ConstService) { }
+
+  ngOnInit(): void {
+    this.cont_service.get_conts().subscribe((conts) => (this.conts = conts));
+  }
+
+}
