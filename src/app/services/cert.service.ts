8b16c1d4efacd27a596933f4812cae7c231cd54e
Hashan <hemachandra.hashan@gmail.com>
2022-06-02 19:58:47 +0530

    add_cert_s() method defined

--- src/app/services/cert.service.ts
+++ src/app/services/cert.service.ts
@@ -1,7 +1,13 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable, } from "rxjs";
 import { Cert } from '../Cert';
+
+const http_options = {
+  headers: new HttpHeaders({
+    'Content-Type' : 'application/json' 
+  })
+}
 
 
 @Injectable({
@@ -14,5 +20,9 @@ export class CertService {
 
   get_certs(): Observable<Cert[]> {
     return this.http.get<Cert[]>(this.api_url)
+  }
+
+  add_cert_s(cert: Cert): Observable<Cert> {
+    return this.http.post<Cert>(this.api_url, cert, http_options )
   }
 }
