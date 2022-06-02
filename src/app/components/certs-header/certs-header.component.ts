211fb7df5de4182f27135bcf5cd616f73080a16c
Hashan <hemachandra.hashan@gmail.com>
2022-06-02 09:53:16 +0530

    Title set and Icon imported

--- src/app/components/certs-header/certs-header.component.ts
+++ src/app/components/certs-header/certs-header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faGraduationCap } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-certs-header',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./certs-header.component.css']
 })
 export class CertsHeaderComponent implements OnInit {
+  title: string = 'Certifications and Awards';
+  faGraduationCap = faGraduationCap;
 
   constructor() { }
 
