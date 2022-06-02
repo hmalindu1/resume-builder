c7c37e6bf0c495a881ebdf05415b6c4881a308b4
Hashan <hemachandra.hashan@gmail.com>
2022-06-02 13:04:42 +0530

    get request for the backend

--- src/app/services/cert.service.ts
+++ src/app/services/cert.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Observable, } from "rxjs";
+import { Cert } from '../Cert';
 
 
 @Injectable({
@@ -10,4 +11,8 @@ export class CertService {
   private api_url = 'http://localhost:5000/certs'
 
   constructor(private http:HttpClient) { }
+
+  get_certs(): Observable<Cert[]> {
+    return this.http.get<Cert[]>(this.api_url)
+  }
 }
