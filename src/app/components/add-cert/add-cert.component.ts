19e8266cf157132d594fbdfafe4c3476799798cd
Hashan <hemachandra.hashan@gmail.com>
2022-06-02 22:31:53 +0530

    form section expand and collapse method

--- src/app/components/add-cert/add-cert.component.ts
+++ src/app/components/add-cert/add-cert.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Cert } from 'src/app/Cert';
+import { Subscription } from "rxjs";
+import { CertUiService } from 'src/app/services/cert-ui.service';
 
 @Component({
   selector: 'app-add-cert',
@@ -10,8 +12,12 @@ export class AddCertComponent implements OnInit {
   @Output() on_add_cert_a: EventEmitter<Cert> = new EventEmitter();
   description!: string;
   url!: string;
+  show_add_cert!: boolean;
+  subscription!: Subscription;
 
-  constructor() { }
+  constructor( private cert_ui_service:CertUiService) { 
+    this.subscription = this.cert_ui_service.on_toggle_add_cert().subscribe((value) => (this.show_add_cert = value))
+  }
 
   ngOnInit(): void {
   }
